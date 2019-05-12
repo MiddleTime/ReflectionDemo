eebe90354fcd1a86f6c29f06538ea2804843dc91
Karl <Karl@DESKTOP-V5FIRD5>
2019-05-12 23:01:46 +0300

    first

---
+++ src/main/java/com/mycompany/demo/SomeClass.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.demo;
+
+
+public class SomeClass {
+    public int someField;
+    public String someStringField;
+    private String somePrivate;
+
+    public String getSomePrivate() {
+        return somePrivate;
+    }
+}
